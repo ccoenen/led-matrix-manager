3069372c7705ccd454fa8be9b53bdc2d74fefa0b
Claudius Coenen <opensource@amenthes.de>
2020-01-26 19:34:04 +0100

    moving this repository to codeberg

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
+Project migrated to codeberg: https://codeberg.org/ccoenen/led-matrix-manager
+
 # LED Matrix Manager
 
 This is a tiny script to manage a HUB75 display, run by a Raspberry Pi via [hzeller's rpi-rgb-led-matrix](https://github.com/hzeller/rpi-rgb-led-matrix).
-
--- ip-display.sh
+++
@@ -1,8 +0,0 @@
-# making sure this is always the current file, if any.
-rm media/ip.png
-
-# generating new file
-/usr/bin/convert -size 192x32 -background black -fill darkgreen -font "DejaVu-Sans-Mono" -pointsize 10 label:"$(ip address show | grep -v "scope host" | awk '/inet6? / {split($2,var,"/*"); print var[1]}')" media/ip.png
-
-# passing on th exit code, just in case.
-exit $?
